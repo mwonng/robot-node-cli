4845cd05fffe0141ffdd8377af79b07917c1a926
Michael Wonng <michael@wonng.com>
2019-04-11 00:19:12 +1000

    remove comment

--- src/lib/Command.js
+++ src/lib/Command.js
@@ -5,46 +5,16 @@ class Command {
     }
 
     static run(command, robot) {
-        let params_reg = /^(?:\w+) (?<x>\d+),(?<y>\d+),(?<direction>\w+)/i;
-        let main_comm  = command.match(/^\w+/gi)[0].toLowerCase();
-
+        let params_reg = /^(?<com>\w+)( (?<x>\d+),(?<y>\d+),(?<direction>\w+))*/i;
         let params = params_reg.test(command) ? params_reg.exec(command).groups : undefined;
-        // let params = params_reg.exec(command).groups;
 
-        // console.log(main_comm);
-        // console.log(params);
-
-        const Action = action(main_comm);
+        const Action = action(params.com.toLowerCase());
         if (Action !== undefined) {
             const run = new Action(robot);
             run.do(params, robot);
         } else {
             console.error("error Action!");
         }
-        // Action;
-
-        // new Action('Move');
-    //     switch (true) {
-    //         case /^PLACE (?<x>\d+),(?<y>\d+),(?<direction>\w+)$/gi.test(command) : {
-    //             let { x, y, direction } = /^PLACE (?<x>\d+),(?<y>\d+),(?<direction>\w+)$/gi.exec(command).groups;
-    //             robot.place(x, y, direction, config.PLACE);
-    //             break;
-    //         }
-    //         case /^MOVE$/gi.test(command) :
-    //             robot.move();
-    //             break;
-    //         case /^LEFT$/gi.test(command) :
-    //             robot.turn(config.LEFT);
-    //             break;
-    //         case /^RIGHT$/gi.test(command) :
-    //             robot.turn(config.RIGHT);
-    //             break;
-    //         case /^REPORT$/gi.test(command) :
-    //             robot.report();
-    //             break;
-    //         default:
-    //             robot.invalid(command) ;
-    //     }
     }
 
 }
